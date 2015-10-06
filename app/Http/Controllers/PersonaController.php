84cea6fa0f8f6125eccbb7bbe665f54573719aa2
Dorit Wittig <dorit.wittig@dmk-ebusiness.de>
2015-10-06 07:56:26 +0200

    [TASK] Merge
    AC: #46

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 
@@ -212,14 +213,14 @@ class PersonaController extends Controller {
 	 * Validierung der Nutzereingaben vor der Speicheroperation.
 	 * TODO Sollte man sicherlich noch einbauen!
 	 */
-// 	public function eingabeKorrekt(){
-// 		$validator = Validator::make(Input::all(), array(
-// 				'title' => '$project_title'
-// 		));
+	public function eingabeKorrekt(){
+		$validator = Validator::make(Input::all(), array(
+				'title' => '$project_title'
+		));
 	
-// 		if($validator->fails){
-// 			return Redirect::action(NewProjectController::index());
-// 		}
-// 		return Redirect::action(NewProjectController::createNewProject());
-// 	}
+		if($validator->fails){
+			return Redirect::action(NewProjectController::index());
+		}
+		return Redirect::action(NewProjectController::createNewProject());
+	}
 }
