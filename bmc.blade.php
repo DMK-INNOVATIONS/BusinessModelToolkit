c6f32c34b8c6cdb89d541e7f932c091bb2f7c085
Dorit Wittig <dorit.wittig@dmk-ebusiness.de>
2015-10-06 07:59:13 +0200

    [TASK] Merge
    AC: #46

---
+++ bmc.blade.php
@@ -0,0 +1,30 @@
+@extends('app') @section('content')
+<div class="container-fluid">
+	<div class=" col-md-10 col-md-offset-1 col-sm-10 col-xs-12 page-header">
+		<h1>Welcome back, {{ Auth::user()->name }}!</h1>
+	</div>
+
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="col-md-4 col-xs-12">
+				<a href="{{ url('/projects') }}" class="thumbnail thumbnail_start">
+					<img class="img-circle" src="{{ asset('img/my_projects.png') }}"
+					alt="My Projects">
+				</a>
+			</div>
+			<div class="col-md-4 col-xs-12">
+				<a href="{{ url('/persona') }}" class="thumbnail thumbnail_start"> <img
+					class="img-circle" src="{{ asset('img/my_personas.png') }}"
+					alt="My Persona">
+				</a>
+			</div>
+			<div class="col-md-4 col-xs-12">
+				<a href="{{ url('/bmc/models') }}" class="thumbnail thumbnail_start"> <img
+					class="img-circle" src="{{ asset('img/my_bmcs.png') }}"
+					alt="My BMC">
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
