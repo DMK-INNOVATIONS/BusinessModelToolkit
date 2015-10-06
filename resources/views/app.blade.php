e63a004471b57c1feca512de93b0b044e31e0cce
Dorit Wittig <dorit.wittig@dmk-ebusiness.de>
2015-10-06 14:18:17 +0200

    [TASK] Anpassung Seitentitel + favicon
    AC: #46

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>BMCounselor</title>
+	<title>Business Model Toolkit</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/MA_Template.css') }}" rel="stylesheet">
+	<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 	
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
