eaf8c896530dfb404cfb7a68079d0baffa559ede
Sorin Burcuta <sorin.burcuta@dmk-ebusiness.de>
2015-11-23 16:48:29 +0100

    #3 BMC Views Css Anpassung, Controller angepasst wurde

--- resources/views/viewBMC.blade.php
+++ resources/views/viewBMC.blade.php
@@ -7,6 +7,7 @@ $posturl = "";
 if (isset ( $user )) :
 	$posturl = $user ['id'];
 
+
  endif;
 
 if ($_SERVER ['SERVER_NAME'] == 'localhost' || $_SERVER ['REMOTE_ADDR'] == '127.0.0.1') {
@@ -16,8 +17,8 @@ if ($_SERVER ['SERVER_NAME'] == 'localhost' || $_SERVER ['REMOTE_ADDR'] == '127.
 }
 ?>
 <div class="help_info">
-	<a class="help-icon" data-toggle="modal" data-target="#helpModal">
-		<span class="icon-question" aria-hidden="true"></span>
+	<a class="help-icon" data-toggle="modal" data-target="#helpModal"> <span
+		class="icon-question" aria-hidden="true"></span>
 	</a>
 </div>
 <div class="container">
@@ -25,7 +26,7 @@ if ($_SERVER ['SERVER_NAME'] == 'localhost' || $_SERVER ['REMOTE_ADDR'] == '127.
 		<div class="col-md-12">
 			<div class="panel panel-default bmc_view_background">
 				<div class="panel-heading bmc_view_Header">
-					<div class="col-md-3 col-sm-3">
+					<div class="col-md-3 col-sm-4">
 						<?php
 						if ($view_type == 'models') {
 							print '<a href="' . $path . '/bmc/models"><button type="button" class="btn btn-default">Back to Model View</button></a>';
@@ -34,7 +35,7 @@ if ($_SERVER ['SERVER_NAME'] == 'localhost' || $_SERVER ['REMOTE_ADDR'] == '127.
 						}
 						?>
 					</div>
-					<div class="col-md-6 col-sm-6">
+					<div class="col-md-7 col-sm-4">
 						<?php
 						if ($owner != 0) {
 							print '<a type="button" class="edit-icon-header" data-toggle="modal" data-target="#titleChangeModal" class="btn btn-default btn-sm"></a>  ';
@@ -42,26 +43,36 @@ if ($_SERVER ['SERVER_NAME'] == 'localhost' || $_SERVER ['REMOTE_ADDR'] == '127.
 						?>
 						<h1 style="margin-top: 10px;"><?php print $bmc_name?></h1>
 					</div>
-					<div class="col-md-3 col-sm-3">
-						<div class="group-icons bmc_group-icons">
+					<div class="col-md-2 col-sm-4 no_padding_right">
+						<div class="pull-right">
+						<div class="status_project">
+							<a class="show-icon" type="button" data-toggle="modal" data-target="#statusChangeModal">
+								<div class="label_{{$bmc_status}} label_status_project">
+									<h5 class="text_status_bmc">{{$bmc_status}}</h5>
+								</div>
+								<!--  <i button type="button" class="icon-triangle glyphicon glyphicon-question-sign"></i>-->
+							</a>
 							<?php
+							
 							// Shows Button in Color of Status
 							$status;
 							switch ($bmc_status) {
 								case 'inWork' :
-									print '<a class="show-icon" type="button" data-toggle="modal" data-target="#statusChangeModal"><i class="icon-minus progress glyphicon glyphicon-minus"></i><i class="bmc_view_status_modal">in progress</i><i button type="button" class="icon-triangle glyphicon glyphicon-question-sign"></i></a>';
+									//print '<a class="show-icon" type="button" data-toggle="modal" data-target="#statusChangeModal"><i class="icon-minus progress glyphicon glyphicon-minus"></i><i class="bmc_view_status_modal">in progress</i><i button type="button" class="icon-triangle glyphicon glyphicon-question-sign"></i></a>';
 									$status = 1;
-									break;
+									//break;
 								case 'approved' :
-									print '<a class="show-icon" type="button" data-toggle="modal" data-target="#statusChangeModal"><i class="icon-minus approved glyphicon glyphicon-minus"></i><i class="bmc_view_status_modal">approved</i><i button type="button" class="icon-triangle glyphicon glyphicon-ok-sign"></i></a>';
+									//print '<a class="show-icon" type="button" data-toggle="modal" data-target="#statusChangeModal"><i class="icon-minus approved glyphicon glyphicon-minus"></i><i class="bmc_view_status_modal">approved</i><i button type="button" class="icon-triangle glyphicon glyphicon-ok-sign"></i></a>';
 									$status = 2;
-									break;
+									//break;
 								case 'rejected' :
-									print '<a class="show-icon" type="button" data-toggle="modal" data-target="#statusChangeModal"><i class="icon-minus rejected glyphicon glyphicon-minus"></i><i class="bmc_view_status_modal">rejected</i><i button type="button" class="icon-triangle glyphicon glyphicon-remove-sign"></i></a>';
+									//print '<a class="show-icon" type="button" data-toggle="modal" data-target="#statusChangeModal"><i class="icon-minus rejected glyphicon glyphicon-minus"></i><i class="bmc_view_status_modal">rejected</i><i button type="button" class="icon-triangle glyphicon glyphicon-remove-sign"></i></a>';
 									$status = 3;
-									break;
+									//break;
 							}
+							
 							?>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -70,172 +81,142 @@ if ($_SERVER ['SERVER_NAME'] == 'localhost' || $_SERVER ['REMOTE_ADDR'] == '127.
 					<div class="viewBMC_body">
 						<div class="row">
 							<div class="col-md-2-4 col-sm-2-4">
-								<div class="row">
-									<div class="col-md-12">
-										<div class="panel panel-default">
-											<div class="panel-heading primary">
-												<h2>Key Partners</h2>
-												<h6 id="KP" style="display: none;">
-													Who are our Key Partners?<br>Who are our Key Suppliers?<br>Which
-													Key Ressources are wo acquairing from partners?<br>Which
-													Key Activities do partners perform?
-												</h6>
-											</div>
-											<div class="services">
-												<a class="add" data-toggle="modal" href="#addPostItModal1"
-													role="button"></a> <a class="more" role="button"
-													onclick="showStuff('KP', this); return false;"></a>
-												<div class="splitline"></div>
-											</div>
-											@include('postItsContent', ['boxId' =>1])
-											@include('postItsViewModal', ['boxId' =>1])
-										</div>
+								<div class="panel panel-default">
+									<div class="panel-heading primary">
+										<h2>Key Partners</h2>
+										<h6 id="KP" style="display: none;">
+											Who are our Key Partners?<br>Who are our Key Suppliers?<br>Which
+											Key Ressources are wo acquairing from partners?<br>Which Key
+											Activities do partners perform?
+										</h6>
 									</div>
+									<div class="services">
+										<a class="add" data-toggle="modal" href="#addPostItModal1"
+											role="button"></a> <a class="more" role="button"
+											onclick="showStuff('KP', this); return false;"></a>
+										<div class="splitline"></div>
+									</div>
+									@include('postItsContent', ['boxId' =>1])
+									@include('postItsViewModal', ['boxId' =>1])
 								</div>
 							</div>
 							<div class="col-md-2-4 col-sm-2-4">
-								<div class="row">
-									<div class="col-md-12">
-										<div class="panel panel-default">
-											<div class="panel-heading primary">
-												<h2>Key Activities</h2>
-												<h6 id="KA" style="display: none;">
-													What Key Activities do our Value Propositions require?<br>Our
-													Distribution Channels?<br>Customer Relationships?<br>Revenue
-													Streams?
-												</h6>
-											</div>
-											<div class="services">
-												<a class="add" data-toggle="modal" href="#addPostItModal2"
-													role="button"></a> <a class="more" role="button"
-													onclick="showStuff('KA', this); return false;"></a>
-												<div class="splitline"></div>
-											</div>
-											@include('postItsContent', ['boxId' =>2])
-											@include('postItsViewModal', ['boxId' =>2])
-										</div>
+								<div class="panel panel-default">
+									<div class="panel-heading primary">
+										<h2>Key Activities</h2>
+										<h6 id="KA" style="display: none;">
+											What Key Activities do our Value Propositions require?<br>Our
+											Distribution Channels?<br>Customer Relationships?<br>Revenue
+											Streams?
+										</h6>
 									</div>
+									<div class="services">
+										<a class="add" data-toggle="modal" href="#addPostItModal2"
+											role="button"></a> <a class="more" role="button"
+											onclick="showStuff('KA', this); return false;"></a>
+										<div class="splitline"></div>
+									</div>
+									@include('postItsContent', ['boxId' =>2])
+									@include('postItsViewModal', ['boxId' =>2])
 								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<div class="panel panel-default">
-											<div class="panel-heading primary">
-												<h2>Key Ressources</h2>
-												<h6 id="KR" style="display: none;">
-													What Key Resources do our Value Propositions require?<br>Our
-													Distribution Channels?<br>Customer Relationships?<br>Revenue
-													Streams?
-												</h6>
-											</div>
-											<div class="services">
-												<a class="add" data-toggle="modal" href="#addPostItModal3"
-													role="button"></a> <a class="more" role="button"
-													onclick="showStuff('KR', this); return false;"></a>
-												<div class="splitline"></div>
-											</div>
-											@include('postItsContent', ['boxId' =>3])
-											@include('postItsViewModal', ['boxId' =>3])
-										</div>
+								<div class="panel panel-default">
+									<div class="panel-heading primary">
+										<h2>Key Ressources</h2>
+										<h6 id="KR" style="display: none;">
+											What Key Resources do our Value Propositions require?<br>Our
+											Distribution Channels?<br>Customer Relationships?<br>Revenue
+											Streams?
+										</h6>
 									</div>
+									<div class="services">
+										<a class="add" data-toggle="modal" href="#addPostItModal3"
+											role="button"></a> <a class="more" role="button"
+											onclick="showStuff('KR', this); return false;"></a>
+										<div class="splitline"></div>
+									</div>
+									@include('postItsContent', ['boxId' =>3])
+									@include('postItsViewModal', ['boxId' =>3])
 								</div>
 							</div>
 							<div class="col-md-2-4 col-sm-2-4">
-								<div class="row">
-									<div class="col-md-12">
-										<div class="panel panel-default">
-											<div class="panel-heading primary">
-												<h2>Value Propositions</h2>
-												<h6 id="VP" style="display: none;">
-													What Value Propositions do we deliver to the Customer?<br>Which
-													one of our Customer\'s problems are we helping to solve?<br>What
-													bundles of products and services are we offering to each
-													Customer Segment?<br>Which customer needs are we
-													satisfying?
-												</h6>
-											</div>
-											<div class="services">
-												<a class="add" data-toggle="modal" href="#addPostItModal4"
-													role="button"></a> <a class="more" role="button"
-													onclick="showStuff('VP', this); return false;"></a>
-												<div class="splitline"></div>
-											</div>
-											@include('postItsContent', ['boxId' =>4])
-											@include('postItsViewModal', ['boxId' =>4])
-										</div>
+								<div class="panel panel-default">
+									<div class="panel-heading primary">
+										<h2>Value Propositions</h2>
+										<h6 id="VP" style="display: none;">
+											What Value Propositions do we deliver to the Customer?<br>Which
+											one of our Customer\'s problems are we helping to solve?<br>What
+											bundles of products and services are we offering to each
+											Customer Segment?<br>Which customer needs are we satisfying?
+										</h6>
 									</div>
+									<div class="services">
+										<a class="add" data-toggle="modal" href="#addPostItModal4"
+											role="button"></a> <a class="more" role="button"
+											onclick="showStuff('VP', this); return false;"></a>
+										<div class="splitline"></div>
+									</div>
+									@include('postItsContent', ['boxId' =>4])
+									@include('postItsViewModal', ['boxId' =>4])
 								</div>
 							</div>
 							<div class="col-md-2-4 col-sm-2-4">
-								<div class="row">
-									<div class="col-md-12">
-										<div class="panel panel-default">
-											<div class="panel-heading primary">
-												<h2>Customer Relationships</h2>
-												<h6 id="CR" style="display: none;">
-													What type of relationship does each of our Customer
-													Segments expect us to establish and maintain with them?<br>Which
-													ones have we established?<br>how are thes integrated with
-													the rest of our business model?<br>How costly are they?
-												</h6>
-											</div>
-											<div class="services">
-												<a class="add" data-toggle="modal" href="#addPostItModal5"
-													role="button"></a> <a class="more" role="button"
-													onclick="showStuff('CR', this); return false;"></a>
-												<div class="splitline"></div>
-											</div>
-											@include('postItsContent', ['boxId' =>5])
-											@include('postItsViewModal', ['boxId' =>5])
-										</div>
+								<div class="panel panel-default">
+									<div class="panel-heading primary">
+										<h2>Customer Relationships</h2>
+										<h6 id="CR" style="display: none;">
+											What type of relationship does each of our Customer Segments
+											expect us to establish and maintain with them?<br>Which ones
+											have we established?<br>how are thes integrated with the rest
+											of our business model?<br>How costly are they?
+										</h6>
 									</div>
+									<div class="services">
+										<a class="add" data-toggle="modal" href="#addPostItModal5"
+											role="button"></a> <a class="more" role="button"
+											onclick="showStuff('CR', this); return false;"></a>
+										<div class="splitline"></div>
+									</div>
+									@include('postItsContent', ['boxId' =>5])
+									@include('postItsViewModal', ['boxId' =>5])
 								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<div class="panel panel-default">
-											<div class="panel-heading primary">
-												<h2>Channels</h2>
-												<h6 id="CH" style="display: none;">
-													Through which Channels do our Customer Segments want to be
-													reached?<br>How are we reaching them now?<br>How are our
-													Channels integrated?<br>Which ones work best?<br>Which ones
-													are most cost-efficient?<br>How are we integrating them
-													with customer routines?
-												</h6>
-											</div>
-											<div class="services">
-												<a class="add" data-toggle="modal" href="#addPostItModal6"
-													role="button"></a> <a class="more" role="button"
-													onclick="showStuff('CH', this); return false;"></a>
-												<div class="splitline"></div>
-											</div>
-											@include('postItsContent', ['boxId' =>6])
-											@include('postItsViewModal', ['boxId' =>6])
-										</div>
+								<div class="panel panel-default">
+									<div class="panel-heading primary">
+										<h2>Channels</h2>
+										<h6 id="CH" style="display: none;">
+											Through which Channels do our Customer Segments want to be
+											reached?<br>How are we reaching them now?<br>How are our
+											Channels integrated?<br>Which ones work best?<br>Which ones
+											are most cost-efficient?<br>How are we integrating them with
+											customer routines?
+										</h6>
 									</div>
+									<div class="services">
+										<a class="add" data-toggle="modal" href="#addPostItModal6"
+											role="button"></a> <a class="more" role="button"
+											onclick="showStuff('CH', this); return false;"></a>
+										<div class="splitline"></div>
+									</div>
+									@include('postItsContent', ['boxId' =>6])
+									@include('postItsViewModal', ['boxId' =>6])
 								</div>
 							</div>
 							<div class="col-md-2-4 col-sm-2-4">
-								<div class="row">
-									<div class="col-md-12">
-										<div class="panel panel-default">
-											<div class="panel-heading primary">
-												<h2>Customer Segments</h2>
-												<h6 id="CS" style="display: none;">
-													For whom are we creating value?<br>Who are our most
-													important customers?
-												</h6>
-											</div>
-											<div class="services">
-												<a class="add" data-toggle="modal" href="#addPostItModal7"
-													role="button"></a> <a class="more" role="button"
-													onclick="showStuff('CS', this); return false;"></a>
-												<div class="splitline"></div>
-											</div>
-											@include('postItsContent', ['boxId' =>7])
-											@include('postItsViewModal', ['boxId' =>7])
-											
-										</div>
+								<div class="panel panel-default">
+									<div class="panel-heading primary">
+										<h2>Customer Segments</h2>
+										<h6 id="CS" style="display: none;">
+											For whom are we creating value?<br>Who are our most important
+											customers?
+										</h6>
 									</div>
+									<div class="services">
+										<a class="add" data-toggle="modal" href="#addPostItModal7"
+											role="button"></a> <a class="more" role="button"
+											onclick="showStuff('CS', this); return false;"></a>
+										<div class="splitline"></div>
+									</div>
+									@include('postItsContent', ['boxId' =>7])
+									@include('postItsViewModal', ['boxId' =>7])
 								</div>
 							</div>
 						</div>
@@ -294,12 +275,28 @@ if ($_SERVER ['SERVER_NAME'] == 'localhost' || $_SERVER ['REMOTE_ADDR'] == '127.
 </div>
 <script>
 $(function ($) {
-	console.log("viewBmc");
-	var obj=$('.bmcViewBackground .col-md-12 .canvas_box');
-	var ob=$.makeArray(obj.length);
+	//console.log("viewBmc");
+	var obj=$('.bmcViewBackground .viewBMC_body > .row .col-md-2-4.col-sm-2-4');
+	//var ob=$.makeArray(obj.length);
+	var max_height=0;
+	var inter_height=0;
 	$.each(obj,function(i,v){
-		console.log(i+" "+$(v).height());
+		inter_height=$(v).height();
+		if(max_height<inter_height){
+			max_height=$(v).height();
+		}
+		//console.log(i+" "+$(v).height());
 		});
+	//console.log('max'+max_height);
+	$.each(obj,function(i,v){
+		if($(v).children().length>1){
+			$(v).children().last().css('height',max_height-$(v).children().height());
+			//console.log(max_height-$(v).children().height());
+		}else{
+			$(v).children().css('height',max_height+4);	
+			}
+		//console.log($(v).children().length);
+	});
 		
 });
 </script>
