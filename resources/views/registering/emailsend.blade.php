9f625f93fe6253f665fa6cef756d1083010a2483
Sorin Burcuta <sorin.burcuta@dmk-ebusiness.de>
2016-04-05 16:06:41 +0200

    AC#497-21 email chenge text

--- resources/views/registering/emailsend.blade.php
+++ resources/views/registering/emailsend.blade.php
@@ -22,13 +22,9 @@
 				display: inline-block;
 			}
 
-			.title {
-				font-size: 72px;
-				margin-bottom: 40px;
-				color: #36444b;
-			}
 			.text {
 				color: #36444b;
+				font-weight: 18px;
 			}
 		</style>
 	</head>
@@ -37,9 +33,8 @@
 			<div class="content">
 				<div class="row">
 					<div clas="col-md-12">
-						<div class="title">Thank you for your registration.</div>
-						<div class="text">You are one click away. Click the following link to authentificate.
-						 <a href="{{ url('/authentificate',['token'=>$token,'email'=>$email]) }}">App Toolkit Builders</a>
+						<div class="text">You&#8217;re one click away. Please verify your email address <?php echo $email; ?>.
+						 <a href="{{ url('/authentificate',['token'=>$token,'email'=>$email]) }}">Link App Toolkit Builders</a>
 						<p>You&#8217;ve received this email because you requested an app.toolkit.builders account with this email address. If you didn&#8217;t intend to, you can ignore this email—the account hasn&#8217;t been created yet.
 Having trouble? Copy and paste this link into your browser and start verification process manually.</p>
 <p>Your support@toolkit.builders.</p>
