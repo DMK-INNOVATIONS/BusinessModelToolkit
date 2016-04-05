c6f8aaea9d8891f82a5d961a5422aadd98694b82
Sorin Burcuta <sorin.burcuta@dmk-ebusiness.de>
2016-04-05 14:42:53 +0200

    AC#497-21 email template symbol

--- resources/views/registering/emailsend.blade.php
+++ resources/views/registering/emailsend.blade.php
@@ -36,7 +36,7 @@
 						<div class="title">Thank you for your registration.</div>
 						<div class="text">You are one click away. Click the following link to authentificate.
 						 <a href="{{ url('/authentificate',['token'=>$token,'email'=>$email]) }}">App Toolkit Builders</a>
-						<p>You&#8217;ve received this email because you requested an app.toolkit.builders account with this email address. If you didn’t intend to, you can ignore this email—the account hasn’t been created yet.
+						<p>You&#8217;ve received this email because you requested an app.toolkit.builders account with this email address. If you didn&#8217;t intend to, you can ignore this email—the account hasn&#8217;t been created yet.
 Having trouble? Copy and paste this link into your browser and start verification process manually.</p>
 <p>Your support@toolkit.builders.</p>
 						
